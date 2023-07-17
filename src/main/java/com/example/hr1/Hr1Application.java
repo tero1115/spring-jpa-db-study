b26a238f0425442f37afc3f2f160b39caad1cdd7
tero1115 <limjisang1115@gmail.com>
2023-07-17 10:09:27 +0900

    init

---
+++ src/main/java/com/example/hr1/Hr1Application.java
@@ -0,0 +1,13 @@
+package com.example.hr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hr1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hr1/Hr1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hr1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hr1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
